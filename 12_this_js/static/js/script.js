67c434449d0ecfe44bcf9f215f8a610372b0cfca
davidtobar-4toC <pockishadow17@gmail.com>
2025-09-30 10:16:53 -0300

    ok

--- 12_this_js/static/js/script.js
+++ 12_this_js/static/js/script.js
@@ -1,4 +1,21 @@
-//crear funcion de prueba this
-function exampleThis() {
-    console.log("elemento clikeado", elemento);
+console.log("page loaded...");
+
+function liked(element){
+    const span = element.querySelector("span");
+    let like = parseInt(span.innerText);
+    like++;
+    span.innerText = like;
+}
+
+
+function login(element){
+    if(element.innerText == "Login"){
+        element.innerText = "Logout";
+    }else{
+        element.innerText = "Login";
+    }
+}
+
+function hide(el){
+    el.remove();
 }
